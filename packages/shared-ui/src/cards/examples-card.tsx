62a834a7b9d5c9b531f4534e61fbde4af05668c1
Mayank Chaudharihttps://mayank-chaudhari.vercel.app <mayank.srmu@gmail.com>
2024-02-20 05:47:11 +0000

    Rebrand 💖 <a href="https://mayank-chaudhari.vercel.app" target="_blank">Mayank Kumar Chaudhari</a> [skip ci]

--- packages/shared-ui/src/cards/examples-card.tsx
+++ packages/shared-ui/src/cards/examples-card.tsx
@@ -4,7 +4,7 @@ export function ExamplesCard() {
 	return (
 		<a
 			className={styles.card}
-			href="https://github.com/react18-tools/turborepo-template"
+			href="https://github.com/mayank1513/zen-store"
 			rel="noopener noreferrer"
 			target="_blank">
 			<h2>
